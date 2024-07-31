ae55daacdee73cc69d37d9976285ebee37b1c4e0
oliver23 <oliver@example.com>
2024-07-31 10:33:02 +0800

    updating 10:32

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 });
 
 app.get('/', (req, res) => {
-    res.send('Proxy server is running');
+    res.send('Proxy server is running!!');
 });
 
 app.get('/home', (req, res) => {
